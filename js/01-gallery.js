d913b4b8f29f0e93f86832e9ed1de02035f16fe2
Maksym Dmitriiev <maximdm22@gmail.com>
2022-07-30 15:52:24 +0300

    555

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -28,6 +28,7 @@ function createImageCardsMarkup (galleryItems) {
     }).join('');
     }
 
+// Эта фунция отвечает за вызов basicLightbox
 
     function onOpenOriginalImage (event) {
       event.preventDefault();
@@ -44,16 +45,10 @@ function createImageCardsMarkup (galleryItems) {
         function onPressKeyESC(evt) {
           if(evt.code ==='Escape'){
             instanse.close();
-          // window.removeEventListener("keydown", onPressKeyESC);
         }
         }
-
-
-
-
-      }
-    
-      }
+        }
+        }
 
 
    
@@ -61,101 +56,3 @@ function createImageCardsMarkup (galleryItems) {
 
 
 
-
-  //   function onCloseOriginalImage (event) {
-  //     window.removeEventListener('click', onClickPress );
-  //     document.body.classList.remove('instanse__image');
-  // }
-
- 
-
-
-    
-    // function onBackdropClick(event) {
-    //     console.log('Клик по backdrop')
-    //     console.log(event.currentTarget)
-    //     console.log(event.target);
-    
-    //     if (event.currentTarget === event.target) {
-    //         console.log('Кликнули именно в бекдроп!!!')
-    //         onCloseModal();
-    //     }
-    // }
-    
-    // function onClickPress (event) {
-    // console.log(event.code);
-    // const ESC_KEY_CODE = 'click';
-    // // const isEscKey = event.code === ESC_KEY_CODE
-    
-    // if (ESC_KEY_CODE){
-    // onCloseModal();
-    // }
-    // }
-    
-// ========================================
-
-
-// const modal = basicLightbox.create(`<img src="${galleryItems.original} width="800" height="600">`);
-// modal.show;
-
-// if(modal.visile()) {
-// window.addEventListener("keydown", onPressKeyESC);
-// window.addEventListener('click', mouseClick);
-// }
-
-
-// function onPressKeyESC(evt) {
-//   if(evt.code ==='Escape'){
-//   modal.close();
-//   window.removeEventListener("keydown", onPressKeyESC);
-// }
-// }
-
-// function mouseClick(evt){
-//   if(evt.cod === 'click'){
-//   modal.close();
-//   window.removeEventListener('click', mouseClick);
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// =========================================
-
-
-//     const gallery = document.querySelector('.gallary');
-
-// const template = ({preview, original, description}) => 
-// ` <div class="gallery__item">
-// <a class="gallery__link" href="${original}">
-// <img
-// class="gallery__image"
-// src="${preview}"
-// alt="${description}"
-// />
-// </a>
-// </div>`;
-
-// const render = (array) => {
-//   const items = array.reduce((acc, item) => (acc += template(item)));
-//   gallery.innerHTML(items);
-
-// };
-
-// render(galleryItems);
-
-// new SimpleLightbox('.gallery a', {
-//   captionDelay: 100,
-//   captionData: "alt",
-// })
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const palettContainer = document.querySelector(".gallery");
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+palettContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+// Эта фунция отвечает за созание разметки
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+    `;
+    })
+    .join("");
+}
+
+// обьявление "SimpleLightbox"
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
